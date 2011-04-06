acceca3db44eabd0fcc11a66f10e43cfc2803531
Sergio Lima <sergio.lima@wiremaze.com>
2011-04-06 15:37:48 +0100

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAPP::Application.config.secret_token = 'd866bb3fe462fd516cddd9fab12627db18680d6e19721bda5e84dd10ecbc882dc7b0ca2c7a4f415cb05270c16db4caa834efd440e64ae97f92c8dbf54fcbd308'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DemoAPP::Application.config.session_store :cookie_store, :key => '_demoAPP_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DemoAPP::Application.config.session_store :active_record_store
